91f70b742dc01f0c122dfe84073ba8a8ab42d5d6
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2024-02-29 18:28:01 +0530

    Add files via upload

---
+++ _16.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class _16
+{
+    public static void main(String[] args)
+    {
+        Scanner scan1 = new Scanner(System.in);
+        int[] a,b; 
+        int inp;
+        a = new int[10];
+        b = new int[10];
+        System.out.print("Enter: ");
+        for(int i = 0; i<10; i++)
+        {
+            inp = scan1.nextInt();
+            a[i] = inp;
+        }
+        for(int i = 0; i<10; i++)
+        {
+            b[i] = a[10 - i - 1];
+        }
+        for(int i = 0; i<10; i++)
+        System.out.print(b[i]+" ");
+        scan1.close();
+    }
+}
